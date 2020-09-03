6fe8cc9706137ab88b1f9e302960fc6bee31b0eb
Alexandr.Abashyn <alexandr.abashyn@gmail.com>
2020-09-03 11:40:18 +0300

    First commit

--- src/index.php
+++ src/index.php
@@ -3,6 +3,7 @@
 echo "Hello, World from Kubernetes! <br>";
 echo "Hello Jenkins X! <br>";
 echo "Hello World X! <br>";
+echo "Hello World XXX! <br>";
 echo '<img border="0" src="https://jenkins-x.io/images/logo.png" width="300" height="300" />';
 
 ?>
